07546fe47eac76ea1d18227edd321bcaf6106333
AnasShaikh5335 <anassashaikh404@gmail.com>
2024-09-13 19:13:04 +0530

    React Animated Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import Navbaar from "./components/Navbaar";
+import Home from "./components/Home";
+import Experience from "./components/Experience";
+import Skills from "./components/Skills";
+import Projects from "./components/projects";
+import Contacts from "./components/Contacts";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+const App = () => {
+  useEffect(() => {
+    Aos.init();
+  }, []);
+  return (
+    <>
+      <Navbaar />
+      <Home />
+      <Experience />
+      <Skills />
+      <Projects />
+      <Contacts />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Contacts.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { FaInstagram } from "react-icons/fa";
+import { CiFacebook } from "react-icons/ci";
+import { CiLinkedin } from "react-icons/ci";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaGithubSquare } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+
+const Contacts = () => {
+  return (
+    <>
+      <div className="container contact" id="contact">
+        <h1>CONTACT ME</h1>
+        <div
+          className="contact-icon"
+          data-aos="zoom-in-up"
+          data-aos-duration="1000"
+        >
+          <a
+            href="https://www.instagram.com"
+            rel="noreferrer"
+            target="_blank"
+            className="items"
+          >
+            <FaInstagram className="icons" />
+          </a>
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noreferrer"
+            className="items"
+          >
+            <CiFacebook className="icons" />
+          </a>
+          <a
+            href="https://www.linkinedin.com"
+            target="_blank"
+            rel="noreferrer"
+            className="items"
+          >
+            <CiLinkedin className="icons" />
+          </a>
+          <a
+            href="https://www.twitter.com"
+            target="_blank"
+            rel="noreferrer"
+            className="items"
+          >
+            <FaSquareXTwitter className="icons" />
+          </a>
+          <a
+            href="https://github.com/AnasShaikh5335/Hospital-Management-System"
+            target="_blank"
+            rel="noreferrer"
+            className="items"
+          >
+            <FaGithubSquare className="icons" />
+          </a>
+          <a
+            href="mailTo:anassashaikh404@gmail.com"
+            rel="noreferrer"
+            className="items"
+          >
+            <SiGmail className="icons" />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contacts;
